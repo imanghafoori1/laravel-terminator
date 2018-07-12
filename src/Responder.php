031f879eaa202715303751c8c330b82261644e54
imanghafoori <imanghafoori1@gmail.com>
2018-07-12 18:46:45 +0430

    rewrite function signature to match the Response interface

--- src/Responder.php
+++ src/Responder.php
@@ -6,58 +6,170 @@ use Illuminate\Contracts\Routing\ResponseFactory;
 
 class Responder implements ResponseFactory
 {
-    public function json(...$args)
+
+    /**
+     * Return a new response from the application.
+     *
+     * @param  string  $content
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\Response
+     */
+    public function make($content = '', $status = 200, array $headers = [])
     {
-        respondWith(response()->json(...$args));
+        respondWith(response()->make($content, $status, $headers));
     }
 
-    public function make(...$args)
+    /**
+     * Return a new view response from the application.
+     *
+     * @param  string  $view
+     * @param  array  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\Response
+     */
+    public function view($view, $data = [], $status = 200, array $headers = [])
     {
-        respondWith(response()->make(...$args));
+        respondWith(response()->view($view, $data, $status, $headers ));
     }
 
-    public function view(...$args)
+    /**
+     * Return a new JSON response from the application.
+     *
+     * @param  string|array  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  int  $options
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function json($data = [], $status = 200, array $headers = [], $options = 0)
     {
-        respondWith(response()->view(...$args));
+        respondWith(response()->json($data, $status, $headers, $options));
+    }
+    /**
+     * Return a new JSONP response from the application.
+     *
+     * @param  string  $callback
+     * @param  string|array  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  int  $options
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function jsonp($callback, $data = [], $status = 200, array $headers = [], $options = 0)
+    {
+        respondWith(response()->jsonp($callback, $data, $status, $headers, $options));
     }
 
-    public function jsonp(...$args)
+    /**
+     * Return a new streamed response from the application.
+     *
+     * @param  \Closure  $callback
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     */
+    public function stream($callback, $status = 200, array $headers = [])
     {
-        respondWith(response()->jsonp(...$args));
+        respondWith(response()->stream($callback, $status, $headers));
     }
 
-    public function stream(...$args)
+    /**
+     * Return a new streamed response as a file download from the application.
+     *
+     * @param  \Closure  $callback
+     * @param  string|null  $name
+     * @param  array  $headers
+     * @param  string|null  $disposition
+     * @return \Symfony\Component\HttpFoundation\StreamedResponse
+     */
+    public function streamDownload($callback, $name = null, array $headers = [], $disposition = 'attachment')
     {
-        respondWith(response()->stream(...$args));
+        respondWith(response()->streamDownload($callback, $name, $headers, $disposition ));
     }
 
-    public function download(...$args)
+    /**
+     * Create a new file download response.
+     *
+     * @param  \SplFileInfo|string  $file
+     * @param  string|null  $name
+     * @param  array  $headers
+     * @param  string|null  $disposition
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     */
+    public function download($file, $name = null, array $headers = [], $disposition = 'attachment')
     {
-        respondWith(response()->download(...$args));
+        respondWith(response()->download($file, $name, $headers, $disposition ));
     }
 
-    public function redirectTo(...$args)
+    /**
+     * Create a new redirect response to the given path.
+     *
+     * @param  string  $path
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectTo($path, $status = 302, $headers = [], $secure = null)
     {
-        respondWith(response()->redirectTo(...$args));
+        respondWith(response()->redirectTo($path, $status, $headers, $secure));
     }
 
-    public function redirectToRoute(...$args)
+    /**
+     * Create a new redirect response to a named route.
+     *
+     * @param  string  $route
+     * @param  array  $parameters
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectToRoute($route, $parameters = [], $status = 302, $headers = [])
     {
-        respondWith(response()->redirectToRoute(...$args));
+        respondWith(response()->redirectToRoute($route, $parameters, $status, $headers));
     }
 
-    public function redirectToAction(...$args)
+    /**
+     * Create a new redirect response to a controller action.
+     *
+     * @param  string  $action
+     * @param  array  $parameters
+     * @param  int  $status
+     * @param  array  $headers
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectToAction($action, $parameters = [], $status = 302, $headers = [])
     {
-        respondWith(response()->redirectToAction(...$args));
+        respondWith(response()->redirectToAction($action, $parameters, $status, $headers ));
     }
 
-    public function redirectGuest(...$args)
+    /**
+     * Create a new redirect response, while putting the current URL in the session.
+     *
+     * @param  string  $path
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectGuest($path, $status = 302, $headers = [], $secure = null)
     {
-        respondWith(response()->redirectGuest(...$args));
+        respondWith(response()->redirectGuest($path, $status, $headers, $secure));
     }
 
-    public function redirectToIntended(...$args)
+    /**
+     * Create a new redirect response to the previously intended location.
+     *
+     * @param  string  $default
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  bool|null  $secure
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function redirectToIntended($default = '/', $status = 302, $headers = [], $secure = null)
     {
-        respondWith(response()->redirectToIntended(...$args));
+        respondWith(response()->redirectToIntended($default, $status, $headers, $secure ));
     }
 }
