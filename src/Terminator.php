e1501589fa29ae961170ee4481e80864f1140f42
imanghafoori <imanghafoori1@gmail.com>
2018-07-13 16:04:14 +0430

    Using standard HttpResponseException behind the scenes to achieve the same goal

--- src/Terminator.php
+++ src/Terminator.php
@@ -2,14 +2,15 @@
 
 namespace ImanGhafoori\Terminator;
 
+use Illuminate\Http\Exceptions\HttpResponseException;
+
 class Terminator
 {
     /**
      * @param $response
-     * @throws \ImanGhafoori\Terminator\TerminateException
      */
     public function respondWith($response)
     {
-        throw new \ImanGhafoori\Terminator\TerminateException($response);
+        throw new HttpResponseException($response);
     }
 }
